a65a391b2410c500bd6793d911a1d66f91bdeabc
softwareprosite <softwareprosite@gmail.com>
2026-06-11 17:14:18 -0400

    Delete backend directory

--- backend/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'ðŸ\u008c„ Sistema de Registro Turístico de Bolivia - Backend API v1.0.0';
-  }
-}
